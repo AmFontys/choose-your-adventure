1c87c0f6dd3087843586c9891d7aa9286d6f8ffa
arenco <479450@student.fontys.nl>
2022-09-15 11:24:42 +0200

    added needed .gradle files

---
+++ Application/ChooseYourAdventure/src/main/java/nl/ChooseYourAdventure/Main.java
@@ -0,0 +1,9 @@
+package nl.ChooseYourAdventure;
+
+import org.springframework.boot.SpringApplication;
+
+public class Main {
+    public static void main(String[] args) {
+        SpringApplication.run(Main.class, args);
+    }
+}
