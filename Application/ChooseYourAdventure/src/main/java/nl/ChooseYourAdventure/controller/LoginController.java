d329116d7f565a1296e2cb790e84c07385a548b8
arenco <479450@student.fontys.nl>
2022-09-15 11:47:25 +0200

    making the spring boot work

---
+++ Application/ChooseYourAdventure/src/main/java/nl/ChooseYourAdventure/controller/LoginController.java
@@ -0,0 +1,14 @@
+package nl.ChooseYourAdventure.controller;
+
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/login")
+@AllArgsConstructor
+public class LoginController {
+}
