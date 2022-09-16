0200dab8bf705904ba908682bd7f89c508d80e87
arenco <479450@student.fontys.nl>
2022-09-16 09:11:39 +0200

    test to see if this fixes the Runner

---
+++ src/main/java/nl/ChooseYourAdventure/configuration/db/DatabaseDataInitializer.java
@@ -0,0 +1,13 @@
+package nl.ChooseYourAdventure.configuration.db;
+
+import lombok.AllArgsConstructor;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
+
+@Component
+@AllArgsConstructor
+public class DatabaseDataInitializer {
+
+
+}
