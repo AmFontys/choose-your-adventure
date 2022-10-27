8de974111a69068cdc6b0122ec2bd43164fc87cd
arenco <479450@student.fontys.nl>
2022-10-27 23:50:07 +0200

    small update so that everything works again

--- Application/ChooseYourAdventure/src/main/java/nl/ChooseYourAdventure/controller/loginController.java
+++ Application/ChooseYourAdventure/src/main/java/nl/ChooseYourAdventure/controller/loginController.java
@@ -20,8 +20,6 @@ import org.springframework.web.bind.annotation.*;
 @AllArgsConstructor
 public class LoginController {
 
-    private userService userService;
-
     private loginService loginService;
 
 
